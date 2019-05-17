05235e89581454b3fcdcb6e7fbb9b7959c544dda
Alex <alexei.zhilin@gmail.com>
2019-05-17 11:28:59 +0300

    Employee controller was enriched with ability to add employees

--- src/main/java/alex/enterprise/employeerestservice/controller/EmployeeController.java
+++ src/main/java/alex/enterprise/employeerestservice/controller/EmployeeController.java
@@ -1,14 +1,14 @@
 package alex.enterprise.employeerestservice.controller;
 
 import alex.enterprise.employeerestservice.entity.Employee;
+import alex.enterprise.employeerestservice.exception.CustomRuntimeException;
 import alex.enterprise.employeerestservice.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static java.util.Objects.isNull;
 
 @RestController
 @RequestMapping("/employee")
@@ -25,5 +25,14 @@ public class EmployeeController {
     @GetMapping("/{id}")
     public Employee getById(@PathVariable Integer id) {
         return employeeService.getById(id);
+    }
+
+    @PutMapping("/add")
+    public Employee add(@RequestBody Employee employee) {
+        if (isNull(employee)) {
+            throw new CustomRuntimeException("Employee cannot be null");
+        }
+
+        return employeeService.add(employee);
     }
 }
--- src/main/java/alex/enterprise/employeerestservice/service/EmployeeService.java
+++ src/main/java/alex/enterprise/employeerestservice/service/EmployeeService.java
@@ -1,11 +1,15 @@
 package alex.enterprise.employeerestservice.service;
 
 import alex.enterprise.employeerestservice.entity.Employee;
+import alex.enterprise.employeerestservice.exception.CustomRuntimeException;
 import alex.enterprise.employeerestservice.repo.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+import static java.util.Objects.isNull;
 
 @Service
 public class EmployeeService {
@@ -18,6 +22,20 @@ public class EmployeeService {
     }
 
     public Employee getById(Integer id) {
-        return employeeDao.findById(id).get();
+        return employeeDao.findById(id).orElseThrow(() -> new CustomRuntimeException(String.format("Employee was not found with particular id: %s", id)));
+    }
+
+    public Employee add(Employee employee) {
+        if (isNull(employee)) {
+            throw new CustomRuntimeException("Employee cannot be null");
+        }
+
+        employeeDao.save(employee);
+
+        Integer id = employee.getId();
+
+        Optional<Employee> employeeOptional = employeeDao.findById(id);
+
+        return employeeOptional.orElseThrow(() -> new CustomRuntimeException(String.format("Stored employee was not found with particular id: %s", id)));
     }
 }
